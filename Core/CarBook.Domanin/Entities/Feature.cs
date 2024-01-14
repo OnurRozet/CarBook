705ea1112a55e8584508f0421d311acb52343171
Onur <onurozet@hotmail.com>
2024-01-14 15:57:25 +0300

    Add project files.

---
+++ Core/CarBook.Domanin/Entities/Feature.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domanin.Entities
+{
+	public class Feature
+	{
+        public int FeatureId { get; set; }
+        public int Name { get; set; }
+        public List<CarFeature> CarFeatures { get; set; }
+    }
+}
